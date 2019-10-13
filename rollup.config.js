655d34e34d64ff8fe1851d8714ce29599aac1701
FAL <falworks.contact@gmail.com>
2019-10-13 14:05:57 +0900

    improve eslint & other configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
+import cleanup from "rollup-plugin-cleanup";
 
 // ---- settings ------------
 
@@ -29,6 +30,8 @@ const additionalBannerComment = `
  */
 `;
 
+const useP5Sound = true;
+
 // --------------------------
 
 const bundledLibraries = bundleFalWorksLibraries
@@ -53,34 +56,32 @@ const typescriptPlugin = typescript({
   useTsconfigDeclarationDir: true
 });
 
-const globals = bundleFalWorksLibraries
-  ? {
-      p5: "p5"
-    }
-  : {
-      p5: "p5",
-      "@fal-works/creative-coding-core": "CreativeCodingCore",
-      "@fal-works/p5-extension": "p5ex"
-    };
+const cleanupPlugin = cleanup({
+  comments: /^\*\*/, // preserve multiline comments
+  extensions: ["ts"]
+});
 
-const external = bundleFalWorksLibraries
-  ? ["p5", "p5/lib/addons/p5.sound"]
-  : [
-      "p5",
-      "p5/lib/addons/p5.sound",
-      "@fal-works/creative-coding-core",
-      "@fal-works/p5-extension"
-    ];
+const globals = {
+  p5: "p5"
+};
+if (!bundleFalWorksLibraries) {
+  globals["@fal-works/creative-coding-core"] = "CreativeCodingCore";
+  globals["@fal-works/p5-extension"] = "p5ex";
+}
 
-const plugins = bundleFalWorksLibraries
-  ? [
-      resolve({
-        extensions: [".mjs"],
-        modulesOnly: true
-      }),
-      typescriptPlugin
-    ]
-  : [typescriptPlugin];
+const external = ["p5"];
+if (useP5Sound) external.push("p5/lib/addons/p5.sound");
+if (!bundleFalWorksLibraries)
+  external.push("@fal-works/creative-coding-core", "@fal-works/p5-extension");
+
+const plugins = [typescriptPlugin, cleanupPlugin];
+if (bundleFalWorksLibraries)
+  plugins.unshift(
+    resolve({
+      extensions: [".mjs"],
+      modulesOnly: true
+    })
+  );
 
 const config = {
   input: "src/main.ts",
